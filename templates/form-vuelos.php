e663167659ef0d865da90d2d7e1bdb5bac105985
migben.com <miguelangel.benitezyanez@gmail.com>
2024-08-14 17:00:17 -0400

    func excel: mejorando front

--- templates/form-vuelos.php
+++ templates/form-vuelos.php
@@ -3,46 +3,50 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12 text-center" style="margin-bottom:40px;">
-					<h2>Lista Desplegable Pais/Ciudad</h2>
+					<h2>Buscador De Vuelos</h2>
 				</div>
 
-				<div class="col-md-8">
+			<div class="col-md-12">
+	
+					<div class="col-md-6">
 						<form class="form-horizontal">
 							<div class="form-group">
-								<label class="col-sm-2 control-label">Country</label>
+								<label class="col-sm-6">Pais Origen</label>
 								<div class="col-sm-10">
 								<div class="ui fluid search selection dropdown gds-cr-semantic" country-data-region-id="gds-cr-four" data-language="en"></div>
 								</div>
 							</div>
 							<div class="form-group">
-								<label for="gds-cr-four" class="col-sm-2 control-label">Region</label>
+								<label for="gds-cr-four" class="col-sm-6">Ciudad Origen</label>
 								<div class="col-sm-10">
 									<select class="form-control" id="gds-cr-four"></select>
 								</div>
 							</div>
 						</form>
-				</div>
+					</div>
 
-
-				<div class="col-md-8">
+					<div class="col-md-6">
 						<form class="form-horizontal">
 							<div class="form-group">
-								<label class="col-sm-2 control-label">Country</label>
+								<label class="col-sm-6">Pais Destino</label>
 								<div class="col-sm-10">
 								<div class="ui fluid search selection dropdown gds-cr-semantic-migben" country-data-region-id="gds-cr-migben" data-language="en"></div>
 								</div>
 							</div>
 							<div class="form-group">
-								<label for="gds-cr-migben" class="col-sm-2 control-label">Region</label>
+								<label for="gds-cr-migben" class="col-sm-6">Ciudad Destino</label>
 								<div class="col-sm-10">
 									<select class="form-control" id="gds-cr-migben"></select>
 								</div>
 							</div>
 						</form>
-				</div>
+					</div>
+			
+			</div>
 
 
-				
+
+
 			</div>
 		</div>
 </html>
